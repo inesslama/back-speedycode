6517b1eecf5f41d14338c927f7b3b6b028e34b00
Iness <inesslama183@gmail.com>
2024-04-29 16:24:13 +0100

    tag6

--- job.groovy
+++ job.groovy
@@ -18,6 +18,9 @@ job('dockerbuild_push') {
             repositoryName('docker1299999/login')
             tag('0.0.1')
             registryCredentials('github_credentials')
+            forcePull(false)
+            createFingerprints(false)
+            skipDecorate()
             
         
         }
