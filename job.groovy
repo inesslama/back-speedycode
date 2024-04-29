ebf804a238bec5fe2a744de6c3f348fd8d9f827e
Iness <inesslama183@gmail.com>
2024-04-29 15:15:48 +0100

    new

--- job.groovy
+++ job.groovy
@@ -1,51 +1,23 @@
-job('dockerbuild_push'){
-    
+job('dockerbuild_push') {
     scm {
         git {
-            
             remote {
                 url('https://github.com/inesslama/back.git') 
                 credentials('github_credentials')
             }
-
             branch('master')
-            
-
-            
-
-            
         }
-
-       
-
-        
-
     }
-
-    triggers{
-        scm('H/5 * * * * ')
-
-    }
-
     
-     steps {
-
-        
-
-       
-        
-        docker.build('docker1299999/login:0.0.1')
-
-        docker.withRegistry('https://registry.hub.docker.com', 'dockerhublogin') {
-
-           docker.image('docker1299999/login:0.0.1').push()
-
-
-        } 
-      
-       
-        
+    triggers {
+        scm('H/5 * * * * ')
     }
 
-
+    steps {
+        // Build Docker image
+        docker.build('docker1299999/login:0.0.1').with {
+            // Push Docker image
+            push()
+        }
+    }
 }
